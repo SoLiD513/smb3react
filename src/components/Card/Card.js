08691c3dd72abdb1f4e062b386ddda13f8dc2e78
Jesse McKinney <jessman51386@gmail.com>
2020-03-28 23:27:19 -0400

    yay

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import "./Card.css";
 
 const Card = props => {
   return (
-    <div onClick={() => props.setClicked(props.id)} className="card col-md-2">
+    <div onClick={() => props.setClicked(props.id)} className="card col-md-3">
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
